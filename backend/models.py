67eab7c7e3b377e84af9b44985a9bb14d06dac75
dyune <david.wang5@mail.mcgill.ca>
2024-11-23 03:59:55 -0500

    Improve database with new schema.

--- backend/models.py
+++ backend/models.py
@@ -2,14 +2,62 @@ import aiosqlite
 
 from database import get_db
 
+# Define all SQL queries
 CREATE_ITEMS_TABLE = """
-CREATE TABLE IF NOT EXISTS items (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT,
-    price REAL NOT NULL
+create table if not exists items
+(
+    id       INTEGER primary key autoincrement,
+    name     TEXT not null,
+    brand    TEXT,
+    category TEXT,
+    unique (name, brand)
 );
 """
+CREATE_PRICES_TABLE = """
+create table if not exists prices
+(
+    id       INTEGER primary key autoincrement,
+    store_id INTEGER not null references stores,
+    item_id  INTEGER not null references items,
+    price    REAL    not null,
+    url      TEXT,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"""
+CREATE_STORES_TABLE = """
+create table if not exists stores
+(
+    id      INTEGER PRIMARY KEY autoincrement,
+    name    TEXT NOT NULL UNIQUE,
+    website TEXT NOT NULL
+);
+"""
+
+INSERT_STORE = "INSERT INTO stores (name, website) VALUES (?, ?)"
+GET_ALL_STORES = "SELECT * FROM stores"
+GET_STORE_BY_ID = "SELECT * FROM stores WHERE id = ?"
+UPDATE_STORE = "UPDATE stores SET name = ?, website = ? WHERE id = ?"
+DELETE_STORE = "DELETE FROM stores WHERE id = ?"
+
+INSERT_PRICE = """
+INSERT INTO prices (store_id, item_id, price, url) 
+VALUES (?, ?, ?, ?)
+"""
+GET_PRICES_BY_ITEM = """
+SELECT p.id, s.name AS store_name, p.price, p.url, p.last_updated 
+FROM prices p
+JOIN stores s ON p.store_id = s.id
+WHERE p.item_id = ?
+"""
+GET_LATEST_PRICE = """
+SELECT p.id, p.price, p.url, p.last_updated 
+FROM prices p
+WHERE p.item_id = ? AND p.store_id = ?
+ORDER BY p.last_updated DESC
+LIMIT 1
+"""
+DELETE_PRICE = "DELETE FROM prices WHERE id = ?"
+
 
 INSERT_ITEM = "INSERT INTO items (name, description, price) VALUES (?, ?, ?)"
 GET_ALL_ITEMS = "SELECT * FROM items"
@@ -17,13 +65,17 @@ GET_ITEM_BY_ID = "SELECT * FROM items WHERE id = ?"
 UPDATE_ITEM = "UPDATE items SET name = ?, description = ?, price = ? WHERE id = ?"
 DELETE_ITEM = "DELETE FROM items WHERE id = ?"
 
-DATABASE_PATH = "./test.db"
+DATABASE_PATH = "grocery_items_data.db"
 
 
 async def initialize_db():
     async with aiosqlite.connect(DATABASE_PATH) as db:
         # Initialize the database schema
         await db.execute(CREATE_ITEMS_TABLE)
+        await db.commit()
+        await db.execute(CREATE_STORES_TABLE)
+        await db.commit()
+        await db.execute(CREATE_PRICES_TABLE)
         await db.commit()
 
 
