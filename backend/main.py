09dfd0e9bf6b183191cb2baab8b54e73521cdb9f
SMLG120 <soccersha120@gmail.com>
2024-11-22 23:30:18 -0500

    .

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
+from typing import Union
+
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def read_root():
     return {"Hello": "World"}
 
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
