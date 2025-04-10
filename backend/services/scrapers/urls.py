046385cc5dc064ff6c5e53574fa3be262c6539b4
dyune <david.wang5@mail.mcgill.ca>
2025-04-09 22:10:15 -0400

    Maintain: added migration script and urls file

---
+++ backend/services/scrapers/urls.py
@@ -0,0 +1,35 @@
+
+
+IGA_URLS = [
+    ("https://www.iga.net/en/online_grocery/instore_bakery", 41),
+    ("https://www.iga.net/en/online_grocery/commercial_bakery", 14),
+    ("https://www.iga.net/en/online_grocery/produce", 55),
+    ("https://www.iga.net/en/online_grocery/home_meal_replacement", 33),
+    ("https://www.iga.net/en/online_grocery/produits_refrigeres", 56),
+    ("https://www.iga.net/en/online_grocery/frozen_grocery", 44),
+    ("https://www.iga.net/en/online_grocery/sushis", 9),
+    ("https://www.iga.net/en/online_grocery/meat", 54),
+    ("https://www.iga.net/en/online_grocery/beverages", 81),
+    ("https://www.iga.net/en/online_grocery/deli_and_cheese", 59),
+    ("https://www.iga.net/en/online_grocery/seafood", 20),
+    ("https://www.iga.net/en/online_grocery/browse/Grocery/Snacks", 87)
+]
+
+SUPER_C_URLS = [
+    ("https://www.superc.ca/en/aisles/fruits-vegetables", 21),
+    ("https://www.superc.ca/en/aisles/dairy-eggs", 31),
+    ("https://www.superc.ca/en/aisles/pantry", 85),
+    ("https://www.superc.ca/en/aisles/cooked-meals", 2),
+    ("https://www.superc.ca/en/aisles/value-pack", 16),
+    ("https://www.superc.ca/en/aisles/beverages", 36),
+    ("https://www.superc.ca/en/aisles/beer-wine", 17),
+    ("https://www.superc.ca/en/aisles/meat-poultry", 13),
+    ("https://www.superc.ca/en/aisles/vegan-vegetarian-food", 13),
+    ("https://www.superc.ca/en/aisles/organic-groceries", 10),
+    ("https://www.superc.ca/en/aisles/snacks", 49),
+    ("https://www.superc.ca/en/aisles/frozen", 29),
+    ("https://www.superc.ca/en/aisles/bread-bakery-products", 14),
+    ("https://www.superc.ca/en/aisles/deli-prepared-meals", 18),
+    ("https://www.superc.ca/en/aisles/fish-seafood", 5),
+    ("https://www.superc.ca/en/aisles/world-cuisine", 8),
+]
